8e882100192c3b32fb020a202112a28f0a6764ba
silvodesigns <kevsilvo@gmail.com>
2020-02-10 02:36:42 -0500

    adding different endpoints

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ server.get('/', (req, res) => {
     res.send('Hello from server api')
 })
 
+server.get('/projects', (req, res) => {
+    //retrieves all projects
+    res.send('Hello from projects api')
+})
+
+server.post('/projects', (req, res) => {
+    //adds a project
+    res.send('Hello from projects api')
+})
+
+server.get('/resources', (req, res) => {
+    //retrieves all resources
+    res.send('Hello from projects api')
+})
+
+server.post('/resources', (req, res) => {
+    //adds a resource
+    res.send('Hello from projects api')
+})
+server.get('/tasks', (req, res) => {
+    //retrieves tasks
+    res.send('Hello from projects api')
+})
+
+server.post('/tasks', (req, res) => {
+    //adds a task
+    res.send('Hello from projects api')
+})
+
+
+
 
 server.listen(4000, () => {
     console.log("API is now up and running")
