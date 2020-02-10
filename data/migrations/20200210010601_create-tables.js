d64b9dcdf4e108ec2ab76f8f35937a1db25cb56b
silvodesigns <kevsilvo@gmail.com>
2020-02-10 01:20:06 -0500

    created tasks table

--- data/migrations/20200210010601_create-tables.js
+++ data/migrations/20200210010601_create-tables.js
@@ -7,6 +7,16 @@ exports.up = function(knex) {
         tbl.boolean('done').notNullable().defaultTo(false);
 
     })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+        tbl.string('description',255)
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+
+    })
   
 };
 
