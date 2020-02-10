bcc92d969705a0dcec2873bf8b9c2f63b520fda7
silvodesigns <kevsilvo@gmail.com>
2020-02-10 01:16:33 -0500

    created projects table

--- data/migrations/20200210010601_create-tables.js
+++ data/migrations/20200210010601_create-tables.js
@@ -1,5 +1,12 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+        tbl.string('description', 255);
+        tbl.boolean('done').notNullable().defaultTo(false);
+
+    })
   
 };
 
