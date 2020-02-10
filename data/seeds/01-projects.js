ec139368019a4e7134ea0dce215bac1ac0e07a17
silvodesigns <kevsilvo@gmail.com>
2020-02-10 01:50:57 -0500

    created projects seeds

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,13 +1,21 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        { 
+          name: "Build an offline application",
+          description: "an application to help you read offline"
+
+        },
+        { 
+          name: "Rebuild a website",
+          description: "redesign a website and make it responsive"
+
+        }
+      
       ]);
     });
 };
